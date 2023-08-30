cb8fc5a54257a0a21d2cb32ae5a572af843779b9
Arturs <janvaris91@gmail.com>
2023-08-30 17:44:58 +0200

    add homework excersises

---
+++ ZADACHA10/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int Message(string message)
+{
+System.Console.Write(message);
+string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result;
+}
+
+int number = Message("Please write 3 digit number > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("You didn't enter 3 digit number, please, enter 3 digit number again");
+    return;
+}
+
+Console.WriteLine($"Entered number '{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Second digit is '{secondRank}'");
+
---
+++ ZADACHA15/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay < 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    } 
+    Console.WriteLine("It is not day of the week");
+    return false;
+}
+
+int weekDay = Prompt("Please enter the day of the week >");
+if (ValidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Yaaay! It is WEEEEEKEND!");
+    }
+    else
+    {
+       Console.WriteLine("NoOoOoO! It is WORKINGday!");
+    }
+}
